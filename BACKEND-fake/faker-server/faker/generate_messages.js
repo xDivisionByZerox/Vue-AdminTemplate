41a7a530f701478d75707dc17be15b34df261a79
xDivisionByZerox <leyla.jaehnig@gmx.de>
2025-03-22 22:14:16 +0100

    refactor: migrate missed random module call

--- BACKEND-fake/faker-server/faker/generate_messages.js
+++ BACKEND-fake/faker-server/faker/generate_messages.js
@@ -3,7 +3,7 @@ const {faker} = require('@faker-js/faker');
 function generateData () {
     var messages = [];
     for (let id = 1; id < 10; id++) {
-        let priority = faker.random.number({min: 1, max: 2});
+        let priority = faker.number.int({min: 1, max: 2});
         let date = faker.date.between("2018-01-01", "2018-07-31").toISOString().split("T")[0];
         let fromId = faker.number.int({min: 1000, max: 9999});
         let message = faker.hacker.phrase();
